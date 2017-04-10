ec18525a4a31f52d0c53a3220f6ea8642848b8fc
Quentin Peten <qpetenpub@gmail.com>
2017-04-10 09:06:51 +0200

    Test 1

--- arduino/ittre-GW-garage/sketch_apr09a/sketch_apr09a.ino
+++ arduino/ittre-GW-garage/sketch_apr09a/sketch_apr09a.ino
@@ -12,7 +12,7 @@
 
 
 unsigned long SLEEP_TIME = 120000; // Sleep time between reports (in milliseconds)
-#define DIGITAL_INPUT_SENSOR 3   // The digital input you attached your motion sensor.  (Only 2 and 3 generates interrupt!)
+#define DIGITAL_INPUT_SENSOR 2   // The digital input you attached your motion sensor.  (Only 2 and 3 generates interrupt!)
 #define CHILD_ID 1   // Id of the sensor child
 
 // Initialize motion message
@@ -21,18 +21,20 @@ unsigned long SLEEP_TIME = 120000; // Sleep time between reports (in millisecond
 void setup()
 {
     pinMode(DIGITAL_INPUT_SENSOR, INPUT);      // sets the motion sensor digital pin as input
+    Serial.begin(115200);
 }
 
 
 void loop()
 {
     // Read digital motion value
-    digitalWrite(14, LOW);
-    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    
     bool tripped = digitalRead(DIGITAL_INPUT_SENSOR) == HIGH;
 
-    Serial.println(tripped);
-    digitalWrite(14, HIGH);
-
-    // Sleep until interrupt comes in on motion sensor. Send update every two minute.
+    if (tripped) {
+      Serial.println(tripped);
+      digitalWrite(LED_BUILTIN, HIGH);
+    }
+    delay(100);
 }
