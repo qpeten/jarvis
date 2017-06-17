9a680aeef6a999f2aa3e06143cfd1190336800ca
Quentin Peten <qpetenpub@gmail.com>
2017-06-17 15:44:50 +0200

    Added MY_NODE_ID

--- arduino/ittre-garage/ittre-garage.ino
+++ arduino/ittre-garage/ittre-garage.ino
@@ -43,7 +43,6 @@
 // Enable debug prints to serial monitor
 //#define MY_DEBUG
 
-
 // Enable and select radio type attached
 #define MY_RADIO_NRF24
 //#define MY_RADIO_RFM69
@@ -52,38 +51,12 @@
 // power your radio separately with a good regulator you can turn up PA level.
 #define MY_RF24_PA_LEVEL RF24_PA_LOW
 
-// Enable serial gateway
-//#define MY_GATEWAY_SERIAL
-
 // Define a lower baud rate for Arduino's running on 8 MHz (Arduino Pro Mini 3.3V & SenseBender)
 #if F_CPU == 8000000L
 #define MY_BAUD_RATE 38400
 #endif
 
-// Enable inclusion mode
-//#define MY_INCLUSION_MODE_FEATURE
-// Enable Inclusion mode button on gateway
-//#define MY_INCLUSION_BUTTON_FEATURE
-
-// Inverses behavior of inclusion button (if using external pullup)
-//#define MY_INCLUSION_BUTTON_EXTERNAL_PULLUP
-
-// Set inclusion mode duration (in seconds)
-//#define MY_INCLUSION_MODE_DURATION 60
-// Digital pin used for inclusion mode button
-//#define MY_INCLUSION_MODE_BUTTON_PIN  3
-
-// Set blinking period
-//#define MY_DEFAULT_LED_BLINK_PERIOD 300
-
-// Inverses the behavior of leds
-//#define MY_WITH_LEDS_BLINKING_INVERSE
-
-// Flash leds on rx/tx/err
-// Uncomment to override default HW configurations
-//#define MY_DEFAULT_ERR_LED_PIN 4  // Error led pin
-//#define MY_DEFAULT_RX_LED_PIN  6  // Receive led pin
-//#define MY_DEFAULT_TX_LED_PIN  5  // the PCB, on board LED
+#define MY_NODE_ID 1
 
 #include <MySensors.h>
 
