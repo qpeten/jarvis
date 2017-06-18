a57a9258f02270dce7192fddf9ab0d894e655e9c
Quentin Peten <qpetenpub@gmail.com>
2017-06-18 15:18:33 +0200

    indentation

--- arduino/ittre-houseFanControl/ittre-houseFanControl.ino
+++ arduino/ittre-houseFanControl/ittre-houseFanControl.ino
@@ -1,24 +1,24 @@
 /**
- * The MySensors Arduino library handles the wireless radio link and protocol
- * between your home built sensors/actuators and HA controller of choice.
- * The sensors forms a self healing radio network with optional repeaters. Each
- * repeater and gateway builds a routing tables in EEPROM which keeps track of the
- * network topology allowing messages to be routed to nodes.
- *
- * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
- * Copyright (C) 2013-2015 Sensnology AB
- * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
- *
- * Documentation: http://www.mysensors.org
- * Support Forum: http://forum.mysensors.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * version 2 as published by the Free Software Foundation.
- *
+   The MySensors Arduino library handles the wireless radio link and protocol
+   between your home built sensors/actuators and HA controller of choice.
+   The sensors forms a self healing radio network with optional repeaters. Each
+   repeater and gateway builds a routing tables in EEPROM which keeps track of the
+   network topology allowing messages to be routed to nodes.
+
+   Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+   Copyright (C) 2013-2015 Sensnology AB
+   Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+
+   Documentation: http://www.mysensors.org
+   Support Forum: http://forum.mysensors.org
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   version 2 as published by the Free Software Foundation.
+
  *******************************
- *
- */
+
+*/
 
 // Enable debug prints to serial monitor
 #define MY_DEBUG
@@ -61,9 +61,9 @@ void setup()
 
 void presentation()
 {
-	// Send the sketch version information to the gateway and Controller
-	sendSketchInfo("houseFanControl", "0.1");
-	present(FAN_FIRST_PIN, S_DIMMER);
+  // Send the sketch version information to the gateway and Controller
+  sendSketchInfo("houseFanControl", "0.1");
+  present(FAN_FIRST_PIN, S_DIMMER);
 }
 
 
@@ -76,7 +76,7 @@ void setSpeed(fanSpeed speed) {
   if (speed == AUTOMATIC) {
     speed = DEFAULT_SPEED;
   }
-  
+
   if (speed == MIN) {
     digitalWrite(FAN_FIRST_PIN, RELAY_OFF);
     digitalWrite(FAN_FIRST_PIN + 1, RELAY_OFF);
@@ -94,9 +94,9 @@ void setSpeed(fanSpeed speed) {
 
 void receive(const MyMessage &message)
 {
-	if (message.destination==MY_NODE_ID && message.sensor==FAN_FIRST_PIN) {
+  if (message.destination == MY_NODE_ID && message.sensor == FAN_FIRST_PIN) {
     setSpeed(percentageToFanSpeed(message.getInt()));
-	}
+  }
 }
 
 fanSpeed percentageToFanSpeed(int percentage) {
