c2d4dfc3bca4931bef13cd940743d69ada2f3cbd
Quentin <quentin@hpBlanc.com>
2017-07-23 17:04:39 +0200

    Going to serial GW

--- arduino/ittre-chQt/ittre-chQt.ino
+++ arduino/ittre-chQt/ittre-chQt.ino
@@ -1,45 +1,45 @@
 /**
-* The MySensors Arduino library handles the wireless radio link and protocol
-* between your home built sensors/actuators and HA controller of choice.
-* The sensors forms a self healing radio network with optional repeaters. Each
-* repeater and gateway builds a routing tables in EEPROM which keeps track of the
-* network topology allowing messages to be routed to nodes.
-*
-* Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
-* Copyright (C) 2013-2015 Sensnology AB
-* Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
-*
-* Documentation: http://www.mysensors.org
-* Support Forum: http://forum.mysensors.org
-*
-* This program is free software; you can redistribute it and/or
-* modify it under the terms of the GNU General Public License
-* version 2 as published by the Free Software Foundation.
-*
-*******************************
-*
-* REVISION HISTORY
-* Version 1.0 - Henrik EKblad
-* Contribution by a-lurker and Anticimex,
-* Contribution by Norbert Truchsess <norbert.truchsess@t-online.de>
-* Contribution by Tomas Hozza <thozza@gmail.com>
-*
-*
-* DESCRIPTION
-* The EthernetGateway sends data received from sensors to the ethernet link.
-* The gateway also accepts input on ethernet interface, which is then sent out to the radio network.
-*
-* The GW code is designed for Arduino 328p / 16MHz.  ATmega168 does not have enough memory to run this program.
-*
-* LED purposes:
-* - To use the feature, uncomment MY_DEFAULT_xxx_LED_PIN in the sketch below
-* - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
-* - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
-* - ERR (red) - fast blink on error during transmission error or recieve crc error
-*
-* See http://www.mysensors.org/build/ethernet_gateway for wiring instructions.
-*
-*/
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * REVISION HISTORY
+ * Version 1.0 - Henrik EKblad
+ * Contribution by a-lurker and Anticimex,
+ * Contribution by Norbert Truchsess <norbert.truchsess@t-online.de>
+ * Contribution by Tomas Hozza <thozza@gmail.com>
+ *
+ *
+ * DESCRIPTION
+ * The EthernetGateway sends data received from sensors to the ethernet link.
+ * The gateway also accepts input on ethernet interface, which is then sent out to the radio network.
+ *
+ * The GW code is designed for Arduino 328p / 16MHz.  ATmega168 does not have enough memory to run this program.
+ *
+ * LED purposes:
+ * - To use the feature, uncomment MY_DEFAULT_xxx_LED_PIN in the sketch below
+ * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
+ * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
+ * - ERR (red) - fast blink on error during transmission error or recieve crc error
+ *
+ * See http://www.mysensors.org/build/ethernet_gateway for wiring instructions.
+ *
+ */
 
 // Enable debug prints to serial monitor
 #define MY_DEBUG
@@ -49,7 +49,9 @@
 //#define MY_RADIO_RFM69
 
 // Enable gateway ethernet module type
-#define MY_GATEWAY_W5100
+//#define MY_GATEWAY_W5100
+
+#define MY_GATEWAY_SERIAL
 
 // W5100 Ethernet module SPI enable (optional if using a shield/module that manages SPI_EN signal)
 //#define MY_W5100_SPI_EN 4
@@ -75,9 +77,9 @@
 // Enable to UDP
 //#define MY_USE_UDP
 
-#define MY_IP_ADDRESS 192,168,1,47   // If this is disabled, DHCP is used to retrieve address
+//#define MY_IP_ADDRESS 192,168,1,47   // If this is disabled, DHCP is used to retrieve address
 // Renewal period if using DHCP
-//#define MY_IP_RENEWAL_INTERVAL 60000
+#define MY_IP_RENEWAL_INTERVAL 60000
 // The port to keep open on node server mode / or port to contact in client mode
 #define MY_PORT 5003
 
@@ -91,16 +93,16 @@
 #define MY_MAC_ADDRESS 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 
 // Enable inclusion mode
-//#define MY_INCLUSION_MODE_FEATURE
+#define MY_INCLUSION_MODE_FEATURE
 // Enable Inclusion mode button on gateway
 //#define MY_INCLUSION_BUTTON_FEATURE
 // Set inclusion mode duration (in seconds)
-//#define MY_INCLUSION_MODE_DURATION 60
+#define MY_INCLUSION_MODE_DURATION 60
 // Digital pin used for inclusion mode button
 //#define MY_INCLUSION_MODE_BUTTON_PIN  3
 
 // Set blinking period
-//#define MY_DEFAULT_LED_BLINK_PERIOD 300
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
 
 // Flash leds on rx/tx/err
 // Uncomment to override default HW configurations
@@ -123,3 +125,4 @@ void setup()
 void loop()
 {
 }
+
