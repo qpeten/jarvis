86a5a4b74be10158947a8048f2fd5c3e2e5a0ed6
Quentin Peten <qpetenpub@gmail.com>
2017-11-05 17:19:01 +0100

    Debug

--- arduino/ittre-garageMQTT/ittre-garageMQTT.ino
+++ arduino/ittre-garageMQTT/ittre-garageMQTT.ino
@@ -149,6 +149,7 @@ bool hasActualLightChanged() {
   if (millis() - lastLightChange > LIGHT_CHANGE_DEBOUNCE_MILLIS && lastLightState != currentSwitchState) {
     lastLightState = currentSwitchState;
     lastLightChange = millis();
+    Serial.println("Light has changed.");
     digitalWrite(PIN_LIGHT_RELAY, false); //We can stop the toggle operation
     return true;
   }
