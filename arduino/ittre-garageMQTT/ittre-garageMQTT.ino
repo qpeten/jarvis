e84f92a4b5e67b7f2970b0354a902315e5eb821a
Quentin Peten <qpetenpub@gmail.com>
2017-11-05 18:35:35 +0100

    Bugfix

--- arduino/ittre-garageMQTT/ittre-garageMQTT.ino
+++ arduino/ittre-garageMQTT/ittre-garageMQTT.ino
@@ -158,7 +158,7 @@ bool hasActualLightChanged() {
     lastLightChange = millis();
     Serial.print("Light has changed. New state: ");
     Serial.println(currentSwitchState);
-    digitalWrite(PIN_LIGHT_RELAY, false); //We can stop the toggle operation
+    //digitalWrite(PIN_LIGHT_RELAY, false); //We can stop the toggle operation
     return true;
   }
   else {
