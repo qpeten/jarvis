f307920b78e999ab504a6b95f5825a1f159ac608
Quentin Peten <qpetenpub@gmail.com>
2017-11-05 14:27:56 +0100

    Changed garage overhead light wiring scheme

--- arduino/ittre-garageMQTT/ittre-garageMQTT.ino
+++ arduino/ittre-garageMQTT/ittre-garageMQTT.ino
@@ -20,25 +20,27 @@
  * A5 = 5V
  */
 #define PIN_LIGHT_RELAY 4
+#define PIN_LIGHT_TOGGLE_MILLIS 25  //Time the relay has to be kept up for the light to actually change
 #define PIN_BOILER_PARENTS_RELAY 5
 #define PIN_BOILER_AYMERIC_RELAY 6
 #define PIN_LIGHT_OUT_RELAY 7
 #define RELAY_ON 1  // GPIO value to write to turn on attached relay
 #define RELAY_OFF 0 // GPIO value to write to turn off attached relay
-#define PIN_SWITCH_INPUT 2 // Pin used to detect the switch state
+#define PIN_LIGHT_INPUT 2 // Pin used to detect the switch state
 #define PIN_CURRENT_SENSOR 14
 #define CURRENT_SENSOR_THRES 8
 #define CURRENT_SENSOR_SMOOTHING_NBR_READINGS 1000
-#define SWITCH_CHANGE_DEBOUNCE_MILLIS 300   //Original 275
-
-static bool backupLightManagement = false; //Will always toggle the light when the switch is pressed, whatever MQTT says
+#define LIGHT_CHANGE_DEBOUNCE_MILLIS 300   //Original 275
+#define CURRENT_SENSOR_DEBOUNCE_MILLIS 14000 //Should be slightly longer than the time it takes to open or close the door
 
 byte mac[] = {0xDE, 0xAA, 0xAA, 0x01, 0x00, 0x00};
 IPAddress ip = (192, 168, 1, 50);
 IPAddress MQTTserver(192, 168, 1, 150);
 
-bool lastSwitchState;
-unsigned long lastSwitchChange = 0;
+bool lastLightState;
+unsigned long lastLightChange = 0;
+unsigned long lastCurrentSensorDetected = 0;
+unsigned long lastLightToggleStart = 0;
 EthernetClient ethClient;
 PubSubClient client(ethClient);
 unsigned long MQTTLastReconnectAttempt = 0;
@@ -50,7 +52,7 @@ void setup() {
   pinMode(PIN_LIGHT_OUT_RELAY, OUTPUT);
   pinMode(PIN_BOILER_PARENTS_RELAY, OUTPUT);
   pinMode(PIN_BOILER_AYMERIC_RELAY, OUTPUT);
-  pinMode(PIN_SWITCH_INPUT, INPUT_PULLUP);
+  pinMode(PIN_LIGHT_INPUT, INPUT_PULLUP);
   pinMode(PIN_CURRENT_SENSOR, INPUT);
 
 
@@ -63,7 +65,7 @@ void setup() {
   setRelay(PIN_LIGHT_OUT_RELAY, true);
   setRelay(PIN_BOILER_PARENTS_RELAY, true);
   setRelay(PIN_BOILER_AYMERIC_RELAY, true);
-  lastSwitchState = digitalRead(PIN_SWITCH_INPUT);
+  lastLightState = digitalRead(PIN_LIGHT_INPUT);
 
   Serial.begin(115200);
 
@@ -84,14 +86,20 @@ void setup() {
 
 void loop() {
   manageMQTTConnexion();
-  manageSwitchToggle();
+  manageActualLightChange();
   manageCurrentSensor();
+  manageLightOutputToggle();
+}
+
+void manageLightOutputToggle() {
+  if (millis() - lastLightToggleStart > PIN_LIGHT_TOGGLE_MILLIS) {
+    digitalWrite(PIN_LIGHT_RELAY, false);
+  }
 }
 
 void setRelay(byte pin, bool state) {
   if (pin == PIN_LIGHT_RELAY) {
-    digitalWrite(PIN_LIGHT_RELAY, state ? RELAY_ON : RELAY_OFF);
-    client.publish("/jarvis/out/state/rez/garage/OverheadLight", getTruthValueFromBool(state));
+    toggleLight(state);
   }
   else if (pin == PIN_BOILER_PARENTS_RELAY) {
     digitalWrite(PIN_BOILER_PARENTS_RELAY, state ? RELAY_ON : RELAY_OFF);
@@ -115,8 +123,8 @@ const char* getTruthValueFromBool (bool input) {
 }
 
 void manageCurrentSensor() {
-  if (currentSensorTriggered()) {
-    client.publish("/jarvis/out/state/rez/garage/GarageDoorMovement","on");
+  if (currentSensorTriggered() && millis() - lastCurrentSensorDetected > CURRENT_SENSOR_DEBOUNCE_MILLIS) {
+    client.publish("/jarvis/out/state/rez/garage/GarageDoorMovement","MOVEMENT");
   }
 }
 
@@ -134,44 +142,34 @@ bool currentSensorTriggered() {
   }
 }
 
-bool hasSwitchChanged() {
-  bool currentSwitchState = digitalRead(PIN_SWITCH_INPUT);
-  if (millis() - lastSwitchChange > SWITCH_CHANGE_DEBOUNCE_MILLIS &&
-      lastSwitchState != currentSwitchState) {
-    lastSwitchState = currentSwitchState;
-    lastSwitchChange = millis();
+bool hasActualLightChanged() {
+  bool currentSwitchState = digitalRead(PIN_LIGHT_INPUT);
+  if (millis() - lastLightChange > LIGHT_CHANGE_DEBOUNCE_MILLIS &&
+      lastLightState != currentSwitchState) {
+    lastLightState = currentSwitchState;
+    lastLightChange = millis();
     return true;
   }
   return false;
 }
 
-void manageSwitchToggle() {
-  if (hasSwitchChanged()) {
-    client.publish("/jarvis/out/command/rez/garage/LightSwitch",2);
-    if (backupLightManagement)
-      toggleLight();
+void manageActualLightChange() {
+  if (hasActualLightChanged()) {
+    client.publish("/jarvis/out/state/rez/garage/OverheadLight", lastLightState ? "ON" : "OFF");
   }
 }
 
-void toggleLight() {
-Serial.println(digitalRead(PIN_LIGHT_RELAY));
-  if (digitalRead(PIN_LIGHT_RELAY) == (RELAY_ON == 1)) {
-    setRelay(PIN_LIGHT_RELAY, false);
-  }
-  else {
-    setRelay(PIN_LIGHT_RELAY, true);
+void toggleLight(bool newState) {
+  if ((digitalRead(PIN_LIGHT_RELAY) == (RELAY_ON == 1)) xor newState) {
+    digitalWrite(PIN_LIGHT_RELAY, true);
+    lastLightToggleStart = millis();
   }
 }
 
 void MQTTMessageReceived(char* topic, byte* payload, unsigned int length) {
   Serial.println("Received message");
-  Serial.println(payload[0]);
   if(strcmp(topic, "/jarvis/in/command/rez/garage/OverheadLight") == 0) {
-    if (payload[0] == '2') {
-      toggleLight();
-    }
-    else
-      setRelay(PIN_LIGHT_RELAY, getTruthValue(payload));
+    setRelay(PIN_LIGHT_RELAY, getTruthValue(payload));
   }
   else if(strcmp(topic, "/jarvis/in/command/rez/garage/ParkingLight") == 0)
     setRelay(PIN_LIGHT_OUT_RELAY, getTruthValue(payload));
